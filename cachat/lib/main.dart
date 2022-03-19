0f0cfa9c250b9bbab262accf87753e6c6dc793b4
Mostafa Essam <57865081+M0stu@users.noreply.github.com>
2022-03-20 00:59:32 +0200

    Assetes v2

--- cachat/lib/main.dart
+++ cachat/lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import './pages/splash_page.dart';
 
 void main() {
-  runApp(SplashPage(
-    key: UniqueKey(),
-    onInitializationComplete: (){},
-  ),
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () {},
+    ),
   );
 }
