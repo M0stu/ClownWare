18a87738e1a16ca352ea88f79affbcb13f993e15
Salmarxaa <201801302@pua.edu.eg>
2022-03-19 14:47:08 -0700

    Done

--- cachat/lib/main.dart
+++ cachat/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed',
+              'You have pulled',
             ),
             Text(
               '$_counter',
