a98ef81cf530aaba2ce56121383b54d3cfe45c4e
khaled-bit <81579155+khaled-bit@users.noreply.github.com>
2022-03-20 00:29:38 +0200

    splash screen edit

--- cachat/lib/pages/splash_page.dart
+++ cachat/lib/pages/splash_page.dart
@@ -1,27 +1,53 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+typedef VoidCallback = void Function();
+
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
   const SplashPage({
     required Key key,
     required this.onInitializationComplete,
   }) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
-    return SplashPageState();
+    // TODO: implement createState
+    return _SplashPageState();
   }
-  class SplashPageState extends State<SplashPage> {
+}
+
+class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-  super.initState();
+    // TODO: implement initState
+    super.initState();
   }
+
+  @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-  title: 'Cachat',
-  theme: ThemeData(
-  backgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
-  scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0)
-  ),
-  );
-  }}
+    return MaterialApp(
+        title: 'chatify',
+        theme: ThemeData(
+          backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+          scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage(''),
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
