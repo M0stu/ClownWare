0ee7caf45141ac9ffc8081f5970963f526340726
M0stu <57865081+M0stu@users.noreply.github.com>
2022-03-22 22:41:22 +0200

    modified main

--- cachat/lib/main.dart
+++ cachat/lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 //Pages
 import './pages/splash_page.dart';
-
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() {
   runApp(
     SplashPage(
-      
       key: UniqueKey(),
       onInitializationComplete: () {},
     ),
