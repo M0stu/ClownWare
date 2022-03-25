5592e5df15b6d7b0319133914db402c472575a9c
Omar Sherif <46868783+omarshherif@users.noreply.github.com>
2022-03-25 14:30:43 +0200

    Calculator Functions

--- calculator/lib/calculator.dart
+++ calculator/lib/calculator.dart
@@ -1,7 +1,24 @@
 library calculator;
 
+import 'dart:math';
+
 /// A Calculator.
 class Calculator {
-  /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  int subOne(int value) => value - 1;
+  int addTwoNumbers(int val1,int val2) => val1+val2;
+  int subTwoNumbers(int val1,int val2) => val1-val2;
+  int mulTwoNumbers(int val1,int val2) => val1*val2;
+  double divTwoNumbers(int val1,int val2) => val1/val2;
+  int modulus(int val1,int val2)=>val1%val2;
+  double sinVal(int value)=>sin(value);
+  double cosVal(int value)=>cos(value);
+  double tanVal(int value)=>tan(value);
+  num powerVal (int val, int exponent)=>pow(val, exponent);
+
+}
+void main(){
+  Calculator calc=Calculator();
+  calc.addOne(4);
+
 }
