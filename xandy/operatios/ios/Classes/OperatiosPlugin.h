eb4e6c390ce4bc85121d3fb13c5b3574f33cb4d3
M0stu <57865081+M0stu@users.noreply.github.com>
2022-03-26 23:27:33 +0200

    updatees in ios

---
+++ xandy/operatios/ios/Classes/OperatiosPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface OperatiosPlugin : NSObject<FlutterPlugin>
+@end
---
+++ xandy/operatios/ios/Classes/OperatiosPlugin.m
@@ -0,0 +1,23 @@
+#import "OperatiosPlugin.h"
+
+@implementation OperatiosPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"operatios"
+            binaryMessenger:[registrar messenger]];
+  OperatiosPlugin* instance = [[OperatiosPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+    
+}
+
+
+
+@end
