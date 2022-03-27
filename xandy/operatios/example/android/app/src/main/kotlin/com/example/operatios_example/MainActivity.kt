bb59d58097ff4a2685852973b84c21aaf6cf98e8
M0stu <57865081+M0stu@users.noreply.github.com>
2022-03-27 02:14:23 +0200

    plugin deleted

--- xandy/operatios/example/android/app/src/main/kotlin/com/example/operatios_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.operatios_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- xandy/operatios/ios/Classes/OperatiosPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface OperatiosPlugin : NSObject<FlutterPlugin>
-@end
