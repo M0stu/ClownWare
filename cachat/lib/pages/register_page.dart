90e26f436b30fabf5c30c708308f1224b0b35471
Mostafa Essam <57865081+M0stu@users.noreply.github.com>
2022-04-01 19:20:17 +0200

    Registation header

---
+++ cachat/lib/pages/register_page.dart
@@ -0,0 +1,11 @@
+//Packages
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+//Services
+import 'package:cachat/services/media_service.dart';
+import 'package:cachat/services/database_service.dart';
+import 'package:cachat/services/cloud_storage_service.dart';
+import 'package:cachat/services/navigation_service.dart';
+//widgets
