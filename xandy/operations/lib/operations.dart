f2156c0eef0a7caf7e84409b5fb7b8f7b5026516
M0stu <57865081+M0stu@users.noreply.github.com>
2022-04-01 14:55:33 +0200

    xandy not xendy

--- xandy/operations/lib/operations.dart
+++ xandy/operations/lib/operations.dart
@@ -1,7 +1,5 @@
 library operations;
-
 import 'dart:math';
-
 /// A Calculator.
 class Calculator {
   int addOne(int value) => value + 1;
@@ -19,11 +17,8 @@ class Calculator {
   int modulus(int val1, int val2) => val1 % val2;
 
   double sinVal(double value) => sin(double.parse(value.toString()));
-
-
   double cosVal(double value) => cos(double.parse(value.toString()));
-
   double tanVal(double value) => tan(double.parse(value.toString()));
-
+  
   num powerVal(num val, num exponent) => pow(val, exponent);
 }
