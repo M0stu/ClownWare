7fe5fc4bbfb0c259e69473f191f69bdf53c06f6d
Mostafa Essam <57865081+M0stu@users.noreply.github.com>
2022-04-02 13:36:10 +0200

    Login Function Created

--- cachat/lib/providers/authentication_provider.dart
+++ cachat/lib/providers/authentication_provider.dart
@@ -1,4 +1,6 @@
 //Packages
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
@@ -7,15 +9,25 @@ import 'package:get_it/get_it.dart';
 import '../services/database_service.dart';
 import '../services/navigation_service.dart';
 
-class AuthenticationProvider extends ChangeNotifier{
+class AuthenticationProvider extends ChangeNotifier {
   late final FirebaseAuth _auth;
   late final NavigationService _navigationService;
-  late final  DatabaseService _databaseService;
+  late final DatabaseService _databaseService;
 
-  AuthenticationProvider(){
-    _auth=FirebaseAuth.instance;
-    _navigationService=GetIt.instance.get<NavigationService>();
-    _databaseService=GetIt.instance.get<DatabaseService>();
+  AuthenticationProvider() {
+    _auth = FirebaseAuth.instance;
+    _navigationService = GetIt.instance.get<NavigationService>();
+    _databaseService = GetIt.instance.get<DatabaseService>();
+  }
 
+  Future<void> loginUsingEmailnPWD(String _email, String _password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+    } on FirebaseAuthException {
+      print("Error Logging in Firebase detected ERROR");
+    } catch (e) {
+      print(e);
+    }
   }
 }
