09d8247a956aec535fc7168e164c376b973dd089
khaled-bit <81579155+khaled-bit@users.noreply.github.com>
2022-04-20 02:31:01 +0200

    Register Page 66%

--- cachat/lib/pages/register_page.dart
+++ cachat/lib/pages/register_page.dart
@@ -8,7 +8,18 @@ import 'package:cachat/services/media_service.dart';
 import 'package:cachat/services/database_service.dart';
 import 'package:cachat/services/cloud_storage_service.dart';
 import 'package:cachat/services/navigation_service.dart';
-//widgets
+//providers
+
+//Widgets
+import '../widgets/custom_input_fields.dart';
+import '../widgets/rounded_button.dart';
+import '../widgets/rounded_image.dart';
+
+//providers
+import '../providers/authentication_provider.dart';
+
+//Pages
+
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -18,12 +29,73 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+ 
+ late double _deviceHeight;
+ late double _deviceWidth;
+
+PlatformFile? _profileImage;
+
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth =MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+       resizeToAvoidBottomInset: false,
+       body: Container(
+         padding: EdgeInsets.symmetric(
+           horizontal:_deviceWidth*0.03,
+           vertical: _deviceHeight*0.02,
+           ) ,
+           height: _deviceHeight*0.98,
+           width: _deviceWidth*0.97,
+           child:Column(
+             mainAxisSize: MainAxisSize.max,
+             mainAxisAlignment: MainAxisAlignment.center,
+             crossAxisAlignment:CrossAxisAlignment.center,
+             children: [
+               _profileImageField(),
+             ],
+           ),
+           ),
+
+    );
+   
+     
+  }
+  Widget _profileImageField(){
+return GestureDetector(
+      onTap: () {
+        GetIt.instance.get<MediaService>().pickImageFromLibrary().then(
+          (_file) {
+            setState(
+              () {
+                _profileImage = _file;
+              },
+            );
+          },
+        );
+      },
+      child: () {
+        if (_profileImage != null) {
+          return RoundedImageFile(
+            key: UniqueKey(),
+            image: _profileImage!,
+            size: _deviceHeight * 0.15,
+          );
+        } else {
+          return RoundedImageNetwork(
+            key: UniqueKey(),
+            imagePath: "https://i.pravatar.cc/150?img=65",
+            size: _deviceHeight * 0.15,
+          );
+        }
+      }(),
+    );
+
   }
 }
