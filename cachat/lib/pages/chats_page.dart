ec7e0e6aad27dbb68482835ac7377d49095bf7ae
Mostafa Essam <57865081+M0stu@users.noreply.github.com>
2022-05-01 08:43:00 +0200

    video 4 chats model complete

--- cachat/lib/pages/chats_page.dart
+++ cachat/lib/pages/chats_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+//provider
+import '../providers/authentication_provider.dart';
+
+//widgets
+import '../widgets/top_bar.dart';
+
 class ChatsPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -11,14 +17,48 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    _auth = Provider.of<AuthenticationProvider>(context);
+
     return _buildUI();
   }
-  Widget _buildUI(){
-    return const Scaffold(
-      backgroundColor: Colors.red,
+
+  Widget _buildUI() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            'Chats',
+            primaryAction: IconButton(
+              icon: Icon(
+                Icons.logout,
+                color: Color.fromRGBO(0, 82, 218, 1.0),
+              ),
+              onPressed: () {
+                _auth.logout();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
