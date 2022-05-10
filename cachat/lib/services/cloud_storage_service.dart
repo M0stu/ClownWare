4d2d2a5a6ea2eac1e05010e5d31970ad8a20ed97
M0stu <57865081+M0stu@users.noreply.github.com>
2022-05-10 07:22:48 +0200

    so far image picker

--- cachat/lib/services/cloud_storage_service.dart
+++ cachat/lib/services/cloud_storage_service.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
+import 'dart:async';
 
 //Packages
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
 
 const String USER_COLLECTION = "Users";
 
@@ -13,14 +18,12 @@ class CloudStorageService {
 
   CloudStorageService() {}
 
-  Future<String?> saveUserImageToStorage(
-      String _uid, PlatformFile _file) async {
+  Future<String?> saveUserImageToStorage(String _uid) async {
     try {
-      Reference _ref =
-          _storage.ref().child('images/users/$_uid/profile.${_file.extension}');
-      UploadTask _task = _ref.putFile(
-        File(_file.path!),
-      );
+      Reference _ref = _storage.ref().child('images/users/$_uid/');
+
+      File f = await getImageFileFromAssets('Assets/img/profile.jpg');
+      UploadTask _task = _ref.putFile(f);
       return await _task.then(
         (_result) => _result.ref.getDownloadURL(),
       );
@@ -45,5 +48,15 @@ class CloudStorageService {
     } catch (e) {
       print(e);
     }
+  }
+
+  Future<File> getImageFileFromAssets(String path) async {
+    final byteData = await rootBundle.load('assets/$path');
+
+    final file = File('${(await getTemporaryDirectory()).path}/$path');
+    await file.writeAsBytes(byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+    return file;
   }
 }
